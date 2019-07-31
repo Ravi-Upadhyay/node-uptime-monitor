b4436b6e9b3f783131a85a90137b8404c59cb254
ravi-upadhyay <raviupadhyay.bkn@gmail.com>
2019-07-31 23:21:24 +0530

    Created http server in index.js that listens on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// console.log('Welcome to node uptime monitor');
+/*
+* Main file for the api
+*/
+
+//Dependencies
+const http = require('http');
+
+//Setting up the server
+const server = http.createServer((request, response) => {
+    response.end('Hello World\n');
+});
+
+//Listening the server on port 3000
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
